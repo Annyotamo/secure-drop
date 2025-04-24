07f0abebd0ef2fda0acaa0762ff4220cb25373f6
Annyotamo Barman <abprogresss@gmail.com>
2025-04-24 11:27:37 +0530

    prod:1.0 commit

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,11 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import Navbar from "../components/Navbar/Navbar";
-import { useState } from "react";
 
 export const rootRoute = createRootRoute({
     component: () => {
-        const [mobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false);
         return (
             <>
-                <Navbar setMobileMenuOpen={setMobileMenuOpen} mobileMenuOpen={mobileMenuOpen} />
+                <Navbar />
                 <Outlet />
             </>
         );
